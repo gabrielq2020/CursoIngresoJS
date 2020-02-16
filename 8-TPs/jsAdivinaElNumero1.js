b0821ab2b6dae15ca185065c8cb984246d4af9c9
Mónica Pascuzzi <gabrielcuatro4@gmail.com>
2020-02-16 20:07:17 -0300

    TP Piedra,papel y tijera

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,26 +7,50 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
+var numeroSecreto;
+var numeroIngresado;
+var contador; 
 var contadorIntentos;
 var max;
 var min;
 
+contador = 1;
+
+
 function comenzar()
 {
-  //Genero el número RANDOM entre 1 y 100
+  //Generar número RANDOM entre 1 y 100
   max = 101;
   min = 1;
   numeroSecreto = Math.floor(Math.random() * (max - min)) + min;
   console.log(numeroSecreto);
 
-		//alert(numeroSecreto );
-	
-
+  //Obtener datos por Id
+  numeroIngresado = document.getElementById("numero").value;
+  
 }
+
+
+
 
 function verificar()
 {
-	
+  //Comparar el numero ingresado con el numero random
+  if (numeroIngresado == numeroSecreto) {
+    alert("Usted es un ganador y en solo " + " intentos");
+  } else {
+    if (numeroIngresado <= numeroSecreto) {
+      alert("Falta para llegar al numero secreto");
+      contadorIntentos = contador + 1;
+      document.getElementById("intentos").value = contadorIntentos;
+    }/* else {
+      alert("Se paso del numero secreto");
+      contadorIntentos = contador + 1;
+      document.getElementById("intentos").value = contadorIntentos;
+
+     
+    }
+    */
+  }
 	
 }
--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,65 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var numeroRandom;
+var max;
+var min;
+
+max = 4;
+min = 1;
 
 function comenzar()
 {
-	
-
+    numeroRandom = Math.floor((Math.random() * (max-min)) + min);
+    console.log(numeroRandom);
 
 }//FIN DE LA FUNCIÓN
+
 function piedra()
 {
+    switch (numeroRandom) {
+        case 1:
+            alert("Usted empato");
+            break;
+        case 2:
+            alert("Usted perdio");
+            break;
+        case 3:
+            alert("Usted gano");
+            break;
+    }
 	
 
 }//FIN DE LA FUNCIÓN
+
 function papel()
 {
-
+    switch (numeroRandom) {
+        case 1:
+            alert("Usted gano");
+            break;
+        case 2:
+            alert("Usted empato");
+            break;
+        case 3:
+            alert("Usted perdio");
+            break;
+    }
 
 }//FIN DE LA FUNCIÓN
+
 function tijera()
 {
-	
+    switch (numeroRandom) {
+        case 1:
+            alert("Usted perdio");
+            break;
+        case 2:
+            alert("Usted gano");
+            break;
+        case 3:
+            alert("Usted empato");
+            break;
+    }
 
 }//FIN DE LA FUNCIÓN
