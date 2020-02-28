ed8f06cab317ed5d236630ec551d381e61df534b
alumno <alumno@utnfra.com.ar>
2020-02-27 22:00:27 -0300

    Clase 27-02-20

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,96 @@
 function mostrar()
 {
+    var respuesta;
+    var marca;
+    var peso;
+    var temperatura;
+    var contadorTemperaturaPar = 0;
+    var primeraVez = true;
+    var maximoMarca;
+    var maximoPeso;
+    var minimoPeso;
+    var contadorMenosCero = 0;
+    var acumuladorPeso = 0;
+    var contadorPeso = 0;
+    var promedioPeso;
+
+
+    do {
+        //Obtener marca y validar
+        do {
+            marca = prompt("Ingrese la marca.");
+        } while (marca == "" || !isNaN(marca));
+
+        //Obtener peso y validar
+        do {
+            peso = prompt("Ingrese un peso entre 1 y 100.");
+            peso = parseInt(peso);            
+        } while (isNaN(peso) || peso < 1 || peso > 100);
+
+        //
+        do {
+            temperatura = prompt("Ingrese una temperatura entre -30 y 30.")
+            temperatura = parseInt(temperatura);
+        } while (isNaN(temperatura) || temperatura < -30 || temperatura > 30);
+
+        //Punto a)                          //Cantidad de temperaturas pares
+        if (temperatura % 2 == 0) {
+            contadorTemperaturaPar++;
+        }
+
+        //Punto b)
+        if (primeraVez) {                   //FLag - Evaluar por primera vez
+            primeraVez = false;             //Cambio a false para que este if solo se ejecute una vez
+            maximoMarca = marca;
+            minimoMarca = marca;
+            maximoPeso = peso;
+            minimoPeso = peso;
+        } else {                            //Punto f)
+            if (peso > maximoPeso) {
+                maximoPeso = peso;
+                maximoMarca = marca;
+            } else if (peso < minimoPeso) { //Punto f)
+                minimoPeso = peso;
+            }
+        }
+
+        //Punto c)
+        if (temperatura < 0) {
+            contadorMenosCero++;
+        }
+
+        //Punto d)
+        contadorPeso++;     //El contador va siempre despues de la validacion de datos, porque sino estoy contando todos los casos, los ingresos correctos y los incorrectos.
+        acumuladorPeso += peso;     //Tanto contador y acumulador si lo pongo despues del while general va a contar una sola vez y va a sumar el ultiimo peso.
+        console.log(contadorPeso);
+
+        respuesta = confirm("Desea continuar?");    //Primero siempre asegurar que el usuario decide la cantidad
+
+    } while (respuesta);
+
+            
+
+//Punto d)      // Los calculos los hago fuera del while general porque hay que esperar que el usuario decida la cantidad de datos a ingresar y ahi recien con todos los datos ingresados hagos los calculos
+if (contadorPeso > 0) {
+    promedioPeso = acumuladorPeso / contadorPeso;
+} else {
+    promedioPeso = alert("No ingreso ningun producto.");
+}
+
+
+document.write("Cantidad de temperaturas pares: " + contadorTemperaturaPar + "<br>" + "Marca del producto mas pesado: " + maximoMarca + "<br>" + "Cantidad de productos que se conservan a menos de 0 grados: " + contadorMenosCero + "<br>" + "Promedio de los pesos de todos los productos: " + promedioPeso + "<br>" + "Peso maximo: " + maximoPeso + "<br>" + "Peso minimo: " + minimoPeso + "<br>");
+
+document.write("Cantidad de temperaturas pares: " + contadorTemperaturaPar + "<br>");
+document.write("Marca del producto mas pesado: " + maximoMarca + "<br>");
+document.write("Cantidad de productos que se conservan a menos de 0 grados: " + contadorMenosCero + "<br>");
+document.write("Promedio de los pesos de todos los productos: " + promedioPeso + "<br>");
+document.write("Peso maximo: " + maximoPeso + "<br>");
+document.write("Peso minimo: " + minimoPeso + "<br>");
+
+
+
 
 }
+
+
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,12 +1,19 @@
-function mostrar()
-{
+function mostrar() {
     var respuesta;
     var numero;
     var letra;
-    var contadorCeros;
-    var contadorPares;
-    var contadorImpares;
-    var acumuladorPositivos;
+    var contadorCeros = 0;
+    var contadorPares = 0;
+    var contadorImpares = 0;
+    var contadorPositivos = 0;
+    var acumuladorPositivos = 0;
+    var acumuladorNegativos = 0;
+    var promedioPositivos;
+    var maximo;
+    var minimo;
+    var primeraVez = true;
+    var maximoLetra;
+    var minimoLetra;
 
     do {
         do {
@@ -28,19 +35,82 @@ function mostrar()
             contadorImpares++;
         }*/
 
-        //Punto c)
 
-        if (numero == 0) {              //Punto c)
-            contadorCeros++;
+
+
+        if (numero % 2 == 0) {      //Punto a)
+            contadorPares++;
         } else {
-            if (numero % 2 == 0) {      //Punto a)
-                contadorPares++;
-            } else {
-                contadorImpares++;        //Punto b)
+            if (numero % 2 == 1) {  //Punto b)
+                contadorImpares++;
+            }
+        }
+
+        //Punto c)
+        if (numero == 0) {
+            contadorCeros++;
+        }
+
+        //Punto d)
+        if (numero > 0) {
+            acumuladorPositivos += numero;
+            contadorPositivos++;
+        }
+
+        //Punto e)
+        if (numero < 0) {
+            acumuladorNegativos += numero;
+        }
+
+        //Punto f)
+        if (primeraVez) {               //Flag
+            primeraVez = false;
+            minimo = numero;
+            maximo = numero;
+            maximoLetra = letra;
+            minimoLetra = letra;
+        } else {
+            if (numero > maximo) {
+                maximo = numero;
+                maximoLetra = letra;
+            }
+            if (numero < minimo) {
+                minimo = numero;
+                minimoLetra = letra;
             }
         }
 
         respuesta = confirm("Desesa continuar?"); //Hacer siempre esto primero
-    } while (respuesta)
+
+    } while (respuesta);
+
+    if (contadorPositivos != 0) {
+        promedioPositivos = acumuladorPositivos / contadorPositivos;
+    } else {
+        promedioPositivos = 0;
+    }
+
+    console.log(promedioPositivos);
+
+    document.write("Cantidad de numeros pares: " + contadorPares + "<br>" + "Cantidad de numeros impares: " + contadorImpares + "<br>" + "Cantidad de ceros: " + contadorCeros + "<br>" + "Promedio de numeros positivos: " + promedioPositivos + "<br>" + "Suma de numeros negativos: " + acumuladorNegativos + "<br>");
+
 
 }
+
+
+/*if (numero % 2 == 0) {      //Punto a)
+    contadorPares++;
+} else if (numero == 0) {
+    contadorCeros;           //Punto c)
+} else {
+    contadorImpares++;      //Punto b)
+}
+
+
+if (numero > 0) {
+    acumuladorPositivos += numero;
+    contadorPositivos++;
+} else if (numero < 0) {
+    acumuladorNegativos += numero;
+}*/
+
