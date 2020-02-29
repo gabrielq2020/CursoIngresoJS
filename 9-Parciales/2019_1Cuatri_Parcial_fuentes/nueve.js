6121fc3b47becd44ddfa326b88d8870f27763233
alumno <alumno@utnfra.com.ar>
2020-02-29 12:34:46 -0300

    Clase repaso sabado 29/02/20

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,48 @@
 function mostrar()
 {
+    //Declaro variables
+   /*  var huesped;
+    var cantidadHuesped;
+    var cantidadEstadia;
+    var formaDePago;
+    var maximoHuesped;
+    var contadorPago;
+    var acumuladorDias = 0;
+    var contadorDias = 0;
+    var respuesta = true
+
+    //Obtener datos por prompt
+      do {
+
+        //Obtener y validar dato de Huesped
+        do {
+          huesped = prompt("Ingrese el nombre del huesped.");
+        } while (!isNaN(huesped) || huesped == "");
+
+        //Obtener y validar dato de cantidad de huespedes
+        do {
+          cantidadHuesped = prompt("Ingrese la cantidad de huespedes");
+        } while (!isNaN(cantidadHuesped) || cantidadHuesped == "");
+
+        //Obtener y validar cantidad de dias de estadia
+        do {
+          cantidadEstadia = prompt("Ingrese la cantidad de dias de estadia");
+        } while (!isNaN(cantidadEstadia) || cantidadEstadia == "");
+
+        //Obtener y validar forma de pago
+        do {
+          formaDePago = prompt("Ingrese la forma de pago");
+          formaDePago = formaDePago.toLowerCase();
+        } while (!isNaN(formaDePago) || formaDePago != "efectivo" && formaDePago != "tarjeta" && formaDePago != "qr"); */
+
+
+
+
+
+
+        respuesta = confirm("Desea ingresar otro huesped?");
+      } while (respuesta);
+
+
 
 }
